2d84c1a097ea02b7ec4dd42863d03626a9c248f6
Carol Greene <carolgreene3@gmail.com>
2018-09-16 02:06:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function produceDrivingRange (range) {
+  return function (start, end) {
+    let street1 = parseInt(start);
+    let street2 = parseInt(end);
+
+
+    let distance = Math.abs(street1 - street2)
+
+    if (distance <= range) {
+      return `within range by ${range - distance}`
+    }
+    else {
+      return `${distance - range} blocks out of range`
+    }
+
+  }
+}
+
+ function produceTipCalculator (percent) {
+   return function (fare) {
+     return fare * percent;
+   }
+ }
+
+function createDriver() {
+  let DriverId = 0;
+  return  class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++DriverId;
+    }
+  }
+}
+
+ 
