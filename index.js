4e5e7d69a556e9687cdb82c12613f73bc81d3a77
Carol Greene <carolgreene3@gmail.com>
2018-11-02 23:20:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-function produceDrivingRange (range) {
-  return function (start, end) {
-    let street1 = parseInt(start);
-    let street2 = parseInt(end);
-    let distance = Math.abs(street1 - street2)
 
-    if (distance <= range) {
-      return `within range by ${range - distance}`
-    }
-    else {
-      return `${distance - range} blocks out of range`
-    }
+function produceDrivingRange(blockRange) {
+  return function(start, end) {                //function has 2 arguments. starting & ending blocks
+    let street1 = parseInt(start)              //set variable = to start block converted to integer
+    let street2 = parseInt(end)                //set variable = to end block converted to integer
+    let distance = Math.abs(street2 - street1)     //set distance variable.  use Math.abs so all answers positive
 
+    if(distance <= blockRange) {                 //calculate is distance is less than blockRange argument
+      let range = blockRange - distance           //is so, set variable to the difference
+      return `within range by ${range}`           //return that it is in range
+    } else {
+      let range = distance - blockRange           //if not, set range to distance - blockRange
+      return `${range} blocks out of range`           //return that it is out of range
+    }
   }
 }
 
- function produceTipCalculator (percent) {
-   return function (fare) {
-     return fare * percent;
-   }
- }
+function produceTipCalculator(percent) {            //argument of percent for tip
+  return function(fare) {                            //return function has argument of the fare
+    return fare * percent                            //return fare * percent
+  }
+}
 
-function createDriver() {
-  let DriverId = 0;
-  return  class {
+function createDriver() {                         //wrap code in createDriver()
+  let DriverId = 0                                 //set DriverId to 0
+  return class {                                   //return class w/constructor info below
     constructor(name) {
-      this.name = name;
-      this.id = ++DriverId;
+      this.name = name
+      this.id = ++DriverId
     }
   }
 }
